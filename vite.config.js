5229aef76dcbab3bd78e9bc4258d4d5a4627265e
Ouail Derghal <ouailderghal1@gmail.com>
2023-04-29 20:03:13 +0200

    Update vite.config.js to run inside Docker container

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,16 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        hmr: {
+            host: '10.0.0.100'
+        },
+        watch: {
+            usePolling: true,
+        }
+    },
+
     plugins: [
         laravel({
             input: [
