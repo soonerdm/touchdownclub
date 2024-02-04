6bc220fec7465fa191a2dbc61587f8d2c05675c4
David Meinke <soonerdm@gmail.com>
2024-02-04 13:10:21 -0600

    updated parking

--- resources/views/parking.blade.php
+++ resources/views/parking.blade.php
@@ -15,13 +15,13 @@
 <p>
     If requesting Asp Ave. Garage Parking Facility, please include your credit card information and mark a second choice in case you aren't drawn.
   </p>
-
+<!--
   <div class="parking_form" style="display: flex;
                                      flex-direction: column;
                                      min-height: 100vh;">
     <script src="https://apps.elfsight.com/p/platform.js" defer></script>
     <div class="elfsight-app-46a4e72f-c615-4ae3-9545-ed7357969fe3" style="flex:1;"></div>
   </div>
-
+-->
 </div>
 @stop
